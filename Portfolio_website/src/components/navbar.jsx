a7133e5e67f6c5fe1699259430f349508dbda6c0
Obonolo Modise <obonolomodd@gmail.com>
2025-12-24 16:34:45 +0200

    added somewhat of a color pallatte and fixed positioning issues.

--- Portfolio_website/src/components/navbar.jsx
+++ Portfolio_website/src/components/navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./css/navbar.css";
 import { RiHome9Fill, RiContactsFill } from "react-icons/ri";
 import { GiSkills } from "react-icons/gi";
-import { FaDiagramProject } from "react-icons/fa6";
+import { FaDiagramProject, FaGithub } from "react-icons/fa6";
 import { VscReferences } from "react-icons/vsc";
 
 
@@ -14,23 +14,27 @@ const Navbar = () => {
                 <ul>
                 <div className="navItem">
                     <RiHome9Fill size="35" className="navIcon" />
-                    <a href="#home">Home</a>
+                    <a href="#home" onClick={() => preventDefault()}>Home</a>
                 </div>
                 <div className="navItem">
                     <GiSkills size="35" className="navIcon" />
-                    <a href="#skills">Skills</a>
+                    <a href="#skills" onClick={()=> preventDefault()}>Skills</a>
                 </div>
                 <div className="navItem">
                     <FaDiagramProject size="35" className="navIcon" />
-                    <a href="#projects">Projects</a>
+                    <a href="#projects" onClick={()=> preventDefault()}>Projects</a>
                 </div>
                 <div className="navItem">
                     <VscReferences size="35" className="navIcon" />
-                    <a href="#references">References</a>
+                    <a href="#references" onClick={()=> preventDefault()}   >References</a>
+                </div>
+                <div className="navItem">
+                    <FaGithub size="35" className="navIcon" />
+                    <a href="#github" onClick={()=> preventDefault()}>GitHub</a>
                 </div>
                 <div className="navItem">
                     <RiContactsFill size="35" className="navIcon" />
-                    <a href="#contact">Contact</a>
+                    <a href="#contact" onClick={()=> preventDefault()}>Contact</a>
                 </div>
 
                 </ul>
